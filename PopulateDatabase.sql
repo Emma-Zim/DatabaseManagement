28b91cf0f067d442aa49a45756636e3da2adcf63
KelbyBSandvick <48974458+KelbyBSandvick@users.noreply.github.com>
2019-12-05 01:07:47 -0800

    SEMICOLON!

--- PopulateDatabase.sql
+++ PopulateDatabase.sql
@@ -13,7 +13,7 @@ insert into Artist (artistId, name) VALUES ('R0001', 'Jaymes Young');
 insert into Genre (genreId, name) VALUES ('G0001', 'Alternative Rock');
 insert into Genre (genreId, name) VALUES ('G0002', 'Indie Pop');
 insert into Label (labelId, name) VALUES ('L0001', 'Atlantic Records');
-insert into Album (albumId, name, recordingType, genreId, labelId) VALUES('A0001', 'Habits Of My Heart', 'Studio', 'G0001', 'L0001')
+insert into Album (albumId, name, recordingType, genreId, labelId) VALUES('A0001', 'Habits Of My Heart', 'Studio', 'G0001', 'L0001');
 insert into Songs (songId, duration, name, lyrics) VALUES ('S0001', '00:03:37', 'Moondust-Stripped', null);
 insert into Songs (songId, duration, name, lyrics) VALUES ('S0002', '00:03:17', 'I\'ll Be Good' , null);
 insert into Songs (songId, duration, name, lyrics) VALUES ('S0003', '00:03:40', 'What Should I Do' , null);
