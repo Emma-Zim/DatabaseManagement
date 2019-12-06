6fac24681f602e88b396c6fdf537a3582df4ff68
Emma <zimmermane@spu.edu>
2019-12-05 19:54:03 -0800

    Fixing query

--- controller.py
+++ controller.py
@@ -13,14 +13,26 @@ class Controller():
 
     def ViewConcertsButton(self, mainFrame, object):
         # execute an sql query
-        retVal = self.connection.SelectConcert()
+        retVal = self.connection.ExecuteQuery("""SELECT B.name, C.location, C.date FROM Concert C
+                                                    JOIN BandConcert BC ON BC.concertId = C.concertId
+                                                    JOIN Band B ON B.bandId = BC.bandId;""")
         #mainFrame.getFrame(object).CreateLabel("Band Location Date")
         for val in retVal:
-            outputString = ""
-            for v in val:
-                if isinstance(v, datetime.date):
-                    v = v.strftime('%y-%m-%d')
-                outputString += v + " "
+            outputString = val[0] + " at " + val[1] + " on " + val[2].strftime('%y-%m-%d')
             mainFrame.getFrame(object).CreateLabel(outputString)
         # output the return on that query on a page
+        mainFrame.showFrame(object)
+
+    def UpdateConcertsPage(self, mainFrame, object):
+        retVal = self.connection.ExecuteQuery("""SELECT B.name, C.location, C.date FROM Concert C
+                                                    JOIN BandConcert BC ON BC.concertId = C.concertId
+                                                    JOIN Band B ON B.bandId = BC.bandId;""")
+        mainFrame.getFrame(object).ShowSelection(retVal)
+        mainFrame.showFrame(object)
+
+    def DeleteConcertsPage(self, mainFrame, object):
+        retVal = self.connection.ExecuteQuery("""SELECT B.name, C.location, C.date FROM Concert C
+                                                    JOIN BandConcert BC ON BC.concertId = C.concertId
+                                                    JOIN Band B ON B.bandId = BC.bandId;""")
+        mainFrame.getFrame(object).ShowSelection(retVal)
         mainFrame.showFrame(object)
